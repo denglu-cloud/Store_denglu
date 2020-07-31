8468b28770bacf9cd68752e03c713746975916a5
denglu <denglu_cloud@aliyun.com>
2020-07-31 21:15:07 +0800

    商品详情-加入购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,5 +1,17 @@
 /**
  * 1 发送请求获取页面数据
+ * 2 点击轮播图 预览大图
+ *        1 给轮播图绑定点击事件
+ *        2 调用小程序的api previewImage
+ * 3 点击，加入购物车
+ *        1 先绑定点击事件
+ *        2 获取缓存中的购物车数据 数组格式
+ *        3 先判断，当前的商品是否已经存在于购物车
+ *        4 已经存在，修改商品的数据，执行购物车数量++，重新把购物车数组填充回缓存中
+ *        5 不存在于购物车的数组中，直接给购物车数组添加一个新元素，新元素带上购买数量属性num,
+ *          重新把购物车数组填充回缓存中
+ *        6 弹出提示
+ *        
  */
 
 
@@ -69,6 +81,34 @@ Page({
                current,
                urls
           })
+     },
+
+     // 点击加入购物车
+     handleCartAdd(){
+          // 1 获取缓存中的购物车 数组
+          // || []：表示转化为数组
+          let cart = wx.getStorageSync("cart") || [];
+          // 2 判断商品对象是否存在于购物车数组中
+          let index = cart.findIndex(v=>v.goods_id === this.GoodsInfo.goods_id);
+          if(index === -1){
+               // 不存在，第一次添加
+               this.GoodsInfo.num = 1;
+               cart.push(this.GoodsInfo);
+          }else{
+               // 4 已经存在购物车数据，执行num++
+               cart[index].num++;
+          }
+          // 5 把购物车重新添加回缓存中
+          wx.setStorageSync("cart",cart);
+          // 6 弹窗提示
+          wx.showToast({
+               title: '加入成功',
+               icon: 'success',
+               // true 防止用户手抖，疯狂点击按钮
+               mask:true,
+
+          })
+
      }
 
 })
